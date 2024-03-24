5b116dce3330d6f92062c5c70f5e19e63e7ecde4
Metta Surendhar <msurendhar8815@gmail.com>
2024-03-25 02:13:24 +0530

    remove the commented code

--- dataPipeline.py
+++ dataPipeline.py
@@ -1,42 +1,7 @@
-# import pandas as pd
-# import requests
-# import json
-# response_API = requests.get('https://api.covid19india.org/state_district_wise.json')
 
-# data = response_API.text
-# print(data)
-# df = pd.DataFrame(response_API.json())
-# print(df)
-# parse_json = json.loads(data)
-# print(parse_json)
-# 'https://api.covid19india.org/state_district_wise.json'
-
-
-#TODO Genre list : 
-# url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
-
-# headers = {
-#     "accept": "application/json",
-#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWY5YzYzNGMwMWIyZjA4ZmI0MDNkZWExNzdiOTIyNSIsInN1YiI6IjY1ZmU5ZTc2MDQ3MzNmMDE3ZGVjNjBmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mvfphI1NIKujcbUUTZ0-dX9pN0sefVmbdj3uwMEHsj0"
-# }
-
-# response = requests.get(url, headers=headers)
-
-# print(response.text)
-
-#TODO popular movie list : 
+#TODO Movie List : 
 #? page 1 - 500 
 #** Things have to see : popularity(popular) , vote average(top rated) , genre_ids(genre) , original_language(lang) , release_date , title , og title ,
-# url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
-
-# headers = {
-#     "accept": "application/json",
-#     "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWY5YzYzNGMwMWIyZjA4ZmI0MDNkZWExNzdiOTIyNSIsInN1YiI6IjY1ZmU5ZTc2MDQ3MzNmMDE3ZGVjNjBmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mvfphI1NIKujcbUUTZ0-dX9pN0sefVmbdj3uwMEHsj0"
-# }
-
-# response = requests.get(url, headers=headers)
-
-# print(response.text)
 
 import requests
 import csv
@@ -147,7 +112,3 @@ if __name__ == '__main__':
   for p in processes:
     p.join()
 
-# print(response.text)
-
-# df = pd.DataFrame(response.json())
-# print(df)
