d8763f9a704f0357ca79654975733ab993b79d03
Metta Surendhar <msurendhar8815@gmail.com>
2024-03-25 01:45:29 +0530

    added the multiprocessing functionality

--- dataPipeline.py
+++ dataPipeline.py
@@ -39,42 +39,79 @@
 # print(response.text)
 
 import requests
-import json
 import csv
 import time
+from multiprocessing import Process
 
-page=1
-headers = {
+
+def extract_api(start,end,headers):
+  data_file = open('./database.csv', 'a', encoding='utf-8', newline='')
+  page=start
+  while page<=end:
+    print(page)
+    url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
+
+    try:
+      response = requests.get(url, headers=headers)
+      json_data = response.json()['results']
+
+      fieldnames = json_data[0].keys()
+      csv_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
+      if page==1:
+        csv_writer.writeheader()
+      
+      for val in json_data: 
+          csv_writer.writerow(val)
+      page=page+1
+    except Exception as e:
+      print(f'error fetching,{e}')
+      # time.sleep(1)
+      page=page
+  data_file.close()
+
+
+  
+
+  
+
+if __name__ == '__main__': 
+
+  headers = {
       "accept": "application/json",
       "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWY5YzYzNGMwMWIyZjA4ZmI0MDNkZWExNzdiOTIyNSIsInN1YiI6IjY1ZmU5ZTc2MDQ3MzNmMDE3ZGVjNjBmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mvfphI1NIKujcbUUTZ0-dX9pN0sefVmbdj3uwMEHsj0"
   }
 
-data_file = open('./database.csv', 'a', encoding='utf-8', newline='')
-
-while page<=500:
-  print(page)
-  url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
-
+  ###  page 1 : 
+  data_file = open('./database.csv', 'a', encoding='utf-8', newline='')
+  url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
   try:
     response = requests.get(url, headers=headers)
     json_data = response.json()['results']
-    print(json_data)
 
     fieldnames = json_data[0].keys()
     csv_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
-    if page==1:
-      csv_writer.writeheader()
-    
+    csv_writer.writeheader()
+
     for val in json_data: 
         csv_writer.writerow(val)
-    page=page+1
-  except:
-     print('error fetching')
-     time.sleep(1)
-     page=page
+  except Exception as e:
+      print(f'error fetching,{e}')
+      time.sleep(1)
+
+  data_file.close()
   
-data_file.close()
-  
+### all pages :
+  processes = []
+
+  p1 = Process(target=extract_api,args=(2,100,headers))
+  processes.append(p1)
+  p1.start()
+  for val in [101,201,301,401]:
+    p = Process(target=extract_api,args=(val,99+val,headers))
+    processes.append(p)
+    p.start()
+  for p in processes:
+    p.join()
 
 # print(response.text)
 
