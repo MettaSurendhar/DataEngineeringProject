942e021a95aa3942bd9cb917d78068de6c55a364
Metta Surendhar <msurendhar8815@gmail.com>
2024-03-25 00:41:13 +0530

    added the extract functionality

--- dataPipeline.py
+++ dataPipeline.py
@@ -1,5 +1,5 @@
 # import pandas as pd
-import requests
+# import requests
 # import json
 # response_API = requests.get('https://api.covid19india.org/state_district_wise.json')
 
@@ -27,7 +27,7 @@ import requests
 #TODO popular movie list : 
 #? page 1 - 500 
 #** Things have to see : popularity(popular) , vote average(top rated) , genre_ids(genre) , original_language(lang) , release_date , title , og title ,
-# url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=100"
+# url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
 
 # headers = {
 #     "accept": "application/json",
@@ -38,3 +38,45 @@ import requests
 
 # print(response.text)
 
+import requests
+import json
+import csv
+import time
+
+page=1
+headers = {
+      "accept": "application/json",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWY5YzYzNGMwMWIyZjA4ZmI0MDNkZWExNzdiOTIyNSIsInN1YiI6IjY1ZmU5ZTc2MDQ3MzNmMDE3ZGVjNjBmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mvfphI1NIKujcbUUTZ0-dX9pN0sefVmbdj3uwMEHsj0"
+  }
+
+data_file = open('./database.csv', 'a', encoding='utf-8', newline='')
+
+while page<=500:
+  print(page)
+  url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
+
+  try:
+    response = requests.get(url, headers=headers)
+    json_data = response.json()['results']
+    print(json_data)
+
+    fieldnames = json_data[0].keys()
+    csv_writer = csv.DictWriter(data_file, fieldnames=fieldnames)
+    if page==1:
+      csv_writer.writeheader()
+    
+    for val in json_data: 
+        csv_writer.writerow(val)
+    page=page+1
+  except:
+     print('error fetching')
+     time.sleep(1)
+     page=page
+  
+data_file.close()
+  
+
+# print(response.text)
+
+# df = pd.DataFrame(response.json())
+# print(df)
