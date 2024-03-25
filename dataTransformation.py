d6c917d2ec1df439a77a0a783d4021aeb8628a16
Metta Surendhar <msurendhar8815@gmail.com>
2024-03-25 18:15:59 +0530

    added the title filter funcitonality in transformation

--- dataTransformation.py
+++ dataTransformation.py
@@ -4,7 +4,6 @@ import pandas as pd
 df = pd.read_csv('./moviesList.csv')
 
 ### movie id filtering:
-
 duplicate_ids = df[df.duplicated(subset=['id'], keep=False)]
 if not duplicate_ids.empty:
     print("Duplicate IDs found in the CSV data. Removing duplicates...")
@@ -14,11 +13,16 @@ if not duplicate_ids.empty:
 else:
     print("No duplicate IDs found in the CSV data.")
 
-### genre id filtering:
-    
+### genre id filtering: 
 print("Updating genre_ids....")
 df['genre_ids'] = df['genre_ids'].str.replace('[', '').str.replace(']', '').str.split(',').str[0]
 df['genre_ids'] = df['genre_ids'].astype(str).replace('', '10770')
 df['genre_ids'] = df['genre_ids'].astype(int)
 df.to_csv('./filteredMoviesList.csv', index=False)
-print('genre_ids  updated!')
+print('Updated genre_ids!')
+
+### Title filtering with 'S' and 'H':
+print("Filtering the titles....")
+filtered_df = df[df['title'].str.startswith(('S', 'H'))]
+filtered_df.to_csv('./filteredMoviesList.csv', index=False)
+print("Filtered the titles!")
