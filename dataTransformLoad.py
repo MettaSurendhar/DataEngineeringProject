14bb56c91c37870c1e25fa073c494a7e57897014
Metta Surendhar <msurendhar8815@gmail.com>
2024-03-25 13:02:06 +0530

    added the load db functionality

---
+++ dataTransformLoad.py
@@ -0,0 +1,126 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+conn = psycopg2.connect("host=localhost dbname=dataEngineering user=postgres password=Suren@19_2004")
+cur = conn.cursor()
+engine = create_engine('postgresql://postgres:Suren%4019_2004@localhost:5432/dataEngineering')
+
+### create tables :
+
+cur.execute("""
+    BEGIN;
+
+
+DROP TABLE IF EXISTS public."Genres";
+
+CREATE TABLE IF NOT EXISTS public."Genres"
+(
+    id integer NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS public."moviesData";
+
+CREATE TABLE IF NOT EXISTS public."moviesData"
+(
+    id integer NOT NULL,
+    title text NOT NULL,
+    overview text,
+    popularity float,
+    "releaseDate" date,
+    rating float,
+    "voteCount" integer,
+    PRIMARY KEY (id),
+    CONSTRAINT "movieId" UNIQUE (id)
+);
+
+DROP TABLE IF EXISTS public.files;
+
+CREATE TABLE IF NOT EXISTS public.files
+(
+    id serial NOT NULL,
+    "movieId" integer NOT NULL,
+    "posterPath" text,
+    "backdropPath" text,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS public."ogMoviesData";
+
+CREATE TABLE IF NOT EXISTS public."ogMoviesData"
+(
+    id serial NOT NULL,
+    "isAdult" boolean,
+    "ogLanguage" text,
+    ogtitle text,
+    "movieId" integer NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS public."movieGenres";
+
+CREATE TABLE IF NOT EXISTS public."movieGenres"
+(
+    id serial NOT NULL,
+    "movieId" integer NOT NULL,
+    "genreId" integer NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.files
+    ADD CONSTRAINT "movieId" FOREIGN KEY ("movieId")
+    REFERENCES public."moviesData" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."ogMoviesData"
+    ADD CONSTRAINT "movieId" FOREIGN KEY ("movieId")
+    REFERENCES public."moviesData" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."movieGenres"
+    ADD CONSTRAINT "genreId" FOREIGN KEY ("genreId")
+    REFERENCES public."Genres" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+
+ALTER TABLE IF EXISTS public."movieGenres"
+    ADD CONSTRAINT "movieId" FOREIGN KEY ("movieId")
+    REFERENCES public."moviesData" (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
+
+END;
+
+""")
+
+conn.commit()
+
+print('created table')
+
+# host=localhost dbname=dataEngineering user=postgres password=Suren@19_2004
+
+
+
+print('created engine')
+df = pd.read_csv('./genreList.csv')
+print('read file')
+
+df.to_sql(name='Genres', con=engine, if_exists='append', index=False)
+print('done!')
+conn.commit()
+
+conn.close()
+# Insert data into the database
+
